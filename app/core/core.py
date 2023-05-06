33162dab83681600c70efe5281e7a1c25cb274e1
Levan Samadashvili <lsama19@freeuni.edu.ge>
2023-05-06 15:48:29 +0400

    remove comments

--- app/core/core.py
+++ app/core/core.py
@@ -11,25 +11,6 @@ class Core:
     account_service: AccountService
 
     def register(self, request: RegisterRequest) -> CoreResponse:
-        # handler = AccountDoesNotExistHandler(
-        #     account_repository=self.account_repository,
-        #     username=request.username,
-        #     password=request.password,
-        # )
-        # register_handler = AccountRegisterHandler(
-        #     account_repository=self.account_repository,
-        #     username=request.username,
-        #     password=request.password,
-        # )
-        # login_handler = LoginHandler(
-        #     application_context=self.application_context,
-        #     token_generator=self.token_generator,
-        #     username=request.username,
-        #     password=request.password,
-        # )
-        #
-        # handler.set_next(register_handler).set_next(login_handler).set_next(NoHandler())
-        # return handler.handle()
         status, account = self.account_service.register(
             username=request.username, password=request.password
         )
@@ -45,20 +26,6 @@ class Core:
         return CoreResponse(status=status, response_content=TokenResponse(token=token))
 
     def login(self, request: LoginRequest) -> CoreResponse:
-        # handler = AccountExistsHandler(
-        #     account_repository=self.account_repository,
-        #     username=request.username,
-        #     password=request.password,
-        # )
-        # login_handler = LoginHandler(
-        #     application_context=self.application_context,
-        #     token_generator=self.token_generator,
-        #     username=request.username,
-        #     password=request.password,
-        # )
-        #
-        # handler.set_next(login_handler).set_next(NoHandler())
-        # return handler.handle()
         status, token = self.account_service.login(
             username=request.username, password=request.password
         )
@@ -68,14 +35,5 @@ class Core:
         return CoreResponse(status=status, response_content=TokenResponse(token=token))
 
     def logout(self, request: LogoutRequest) -> CoreResponse:
-        # handler = UserLoggedInHandler(
-        #     application_context=self.application_context, token=request.token
-        # )
-        # logout_handler = LogoutHandler(
-        #     application_context=self.application_context, token=request.token
-        # )
-        #
-        # handler.set_next(logout_handler).set_next(NoHandler())
-        # return handler.handle()
         status = self.account_service.logout(token=request.token)
         return CoreResponse(status=status)
