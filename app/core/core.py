c397bd6f174b4321b596980c87e0f0800b8121e6
Levan Samadashvili <lsama19@freeuni.edu.ge>
2023-05-06 16:06:15 +0400

    refactor login/logout

--- app/core/core.py
+++ app/core/core.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 from app.core.constants import Status
 from app.core.requests import LoginRequest, LogoutRequest, RegisterRequest
-from app.core.responses import CoreResponse, TokenResponse
+from app.core.responses import CoreResponse, ResponseContent, TokenResponse
 from app.core.services.account_service import AccountService
 
 
@@ -14,25 +14,23 @@ class Core:
         status, account = self.account_service.register(
             username=request.username, password=request.password
         )
+
         if status != Status.OK or account is None:
             return CoreResponse(status=status)
 
-        status, token = self.account_service.login(
-            username=account.username, password=account.password
+        return self.login(
+            request=LoginRequest(username=account.username, password=account.password)
         )
-        if status != Status.OK or token is None:
-            return CoreResponse(status=status)
-
-        return CoreResponse(status=status, response_content=TokenResponse(token=token))
 
     def login(self, request: LoginRequest) -> CoreResponse:
         status, token = self.account_service.login(
             username=request.username, password=request.password
         )
-        if status != Status.OK or token is None:
-            return CoreResponse(status=status)
 
-        return CoreResponse(status=status, response_content=TokenResponse(token=token))
+        token_response = (
+            ResponseContent() if token is None else TokenResponse(token=token)
+        )
+        return CoreResponse(status=status, response_content=token_response)
 
     def logout(self, request: LogoutRequest) -> CoreResponse:
         status = self.account_service.logout(token=request.token)
